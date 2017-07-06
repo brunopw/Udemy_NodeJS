c00c1f10e2745529836d772e2c387038b9630957
brunopw <bruno.warwezeniak@gmail.com>
2017-07-05 23:37:23 -0300

    Instalação do express

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/tecnologia', function(req, res) {
+
+    res.render("secao/tecnologia");
+});
+
+
+app.get('/', function(req, res) {
+
+    res.render("secao/home");
+});
+
+
+
+app.listen(3000, function() {
+
+    console.log("Servidor rodando com express");
+});
