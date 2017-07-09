bcb23ae5f47feb49700b5788f514ec5aed23e2e5
brunopw <bruno.warwezeniak@gmail.com>
2017-07-09 19:13:13 -0300

    Consign adicionado e configurado. Model de noticias criado.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//App
 var app = require('./config/server');
 
-//Routes
-var rotaHome = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaFormAddNoticia = require('./app/routes/form_add_noticia')(app);
+//Routes Manual
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaFormAddNoticia = require('./app/routes/form_add_noticia')(app);
 
 //Start Server
 app.listen(3000, function() {
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function() {
+var connMySQL = function() {
+    console.log('Conexão com banco de dados solicitada.');
     return mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : '1234',
         database : 'portal_noticias'
     });
+};
+
+module.exports = function(){
+    console.log('O autoload carregou o módulo de xonexão com o banco de dados.');
+    return connMySQL;
 };
