cadcdde09bb9ecf75550c7b3deca8184c08be9c8
brunopw <bruno.warwezeniak@gmail.com>
2017-07-16 22:56:08 -0300

    Finalizada implementação básica do Chat.

--- Multiroom_Chat/app/routes/chat.js
+++ Multiroom_Chat/app/routes/chat.js
@@ -1,9 +1,12 @@
 module.exports = function(application) {
+
     application.post('/chat', function(req, res) {
-        res.render('chat');
+        
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
 
     application.get('/chat', function(req, res) {
-        res.render('chat');
+
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
 };
