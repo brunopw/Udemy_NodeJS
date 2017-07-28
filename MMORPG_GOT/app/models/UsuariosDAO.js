ad63e3d04181dce95037c9413c19ef5db49ceedb
brunopw <bruno.warwzeniak@gmail.com>
2017-07-27 21:50:20 -0300

    Implementado controle de session por cookie.

--- MMORPG_GOT/app/models/UsuariosDAO.js
+++ MMORPG_GOT/app/models/UsuariosDAO.js
@@ -12,6 +12,35 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario) {
     });
 };
 
+UsuariosDAO.prototype.autenticar = function(usuario, req, res) {
+    this._connection.open( function(err, mongoclient) {
+        mongoclient.collection("usuarios", function(err, collection) {
+            collection.find(usuario).toArray(function(err, result) {
+                
+                if(result[0] != undefined){
+
+                    req.session.autorizado = true;
+
+                    req.session.usuario = result[0].nome;
+                    req.session.casa = result[0].casa;
+                }
+
+                if(req.session.autorizado) {
+                    res.redirect("jogo");
+                }else {
+                    res.render("index",{
+                        validacao: {msg: 'usuário não encontrado no db.'},
+                        dadosForm: usuario
+                    });
+                }
+
+            });
+
+            mongoclient.close();
+        });
+    });
+};
+
 module.exports = function() {
     return UsuariosDAO;
 };
